16c8af7c2c26b9431166058d4b4d4b62d33efa6c
jacobaperez <jacobanthony.perez@gmail.com>
2017-10-16 20:12:05 -0700

    work in progress

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,12 +1,27 @@
 var VideoListView = Backbone.View.extend({
 
+  el: '.list',
+
+  initialize: function() {
+    this.videos = window.exampleVideoData;
+    this.render();
+  },
 
   render: function() {
+    console.log("VideoListView Render Ran");
     this.$el.children().detach();
-    this.$el.html(this.template());
+    // this.$el.html(this.template());
+    // new VideoListEntryView();
+    console.log(this.videos);
+    for (var i = 0; i < this.videos.length; i++) {
+      console.log(this.videos[i]);
+          // this.$el.html(this.template(this.model.attributes));
+      $(this.el).append(VideoListEntryView({model: this.videos[i]}));
+    }
     return this;
   },
 
   template: templateURL('src/templates/videoList.html')
+
 
 });
